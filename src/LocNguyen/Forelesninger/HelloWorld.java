31e238bd9a7c03e23b486419e427a282bad04ba4
Snackme <s351928@oslomet.no>
2021-09-24 19:39:20 +0100

    initial commit 2 på msi

--- src/LocNguyen/Forelesninger/HelloWorld.java
+++ src/LocNguyen/Forelesninger/HelloWorld.java
@@ -2,6 +2,6 @@ package LocNguyen.Forelesninger;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world algdat");
+        System.out.println("hello world algdat på MSI :D");
     }
 }
